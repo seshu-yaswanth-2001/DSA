5f1d8af8c51e0f1a538a874c36755e08a62636ee
Seshu Yaswanth Reddy <seshuyaswanthreddy.k@gmail.com>
2025-07-22 16:02:12 +0530

    optimized

--- Basic Math/primeNumbers.js
+++ Basic Math/primeNumbers.js
@@ -31,3 +31,19 @@ const res = primeNumber(21);
 console.log(res);
 
 // time complexity: O(sqrt(n))
+
+function primeNumbers(num) {
+  let count = 0;
+  for(let i = 0; i <= Math.sqrt(num); i++) {
+    if(num % i === 0) {
+      count = count + 1;
+      
+      if(num / i !== i) {
+        count = count + 1
+      }
+    }
+  }
+  return count === 2 ? "Prime Number" : "Not Prime Number"
+}
+const result = primeNumbers(36);
+console.log(result)
