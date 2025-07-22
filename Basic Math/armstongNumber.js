c25dfd714780d02b5388a5461e0f270d1b510152
Seshu Yaswanth Reddy <seshuyaswanthreddy.k@gmail.com>
2025-07-22 16:50:51 +0530

    bug fixed

--- Basic Math/armstongNumber.js
+++ Basic Math/armstongNumber.js
@@ -1,9 +1,10 @@
 function armstronNumber(n) {
+  const digits = n.toString().length;
   const dup = n;
   let sum = 0;
   while (n > 0) {
     let lastDigit = n % 10;
-    sum = sum + lastDigit ** 3;
+    sum = sum + lastDigit ** digits;
     n = Math.floor(n / 10);
   }
   return dup === sum;
